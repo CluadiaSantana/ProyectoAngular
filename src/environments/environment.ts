ca8120c55aae0b840baed732e813c626c34088da
CluadiaSantana <is726396@iteso.mx>
2021-11-08 00:12:49 -0600

    environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiHost: "http://localhost:3001",
+  apiPath: "/api",
+  authPath: "/auth"
 };
 
 /*
