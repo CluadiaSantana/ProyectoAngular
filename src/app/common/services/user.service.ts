972fd38064658a19db33821473cd9885af7a8193
KAMZ13 <84303083+KAMZ13@users.noreply.github.com>
2021-11-08 02:27:07 -0600

    get all or get by id USERS
    GET calls created.

--- src/app/common/services/user.service.ts
+++ src/app/common/services/user.service.ts
@@ -1,19 +1,54 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { AuthenticationService } from './authentication.service';
+
+import { environment } from './../../../environments/environment';
+
+
+
 @Injectable({
+
   providedIn: 'root'
+
 })
+
 export class UserService {
 
-  constructor() { }
 
 
-  getUsers():Promise <any>{
-    return new Promise ((sucess)=>{
-      
-    })
-  }
+  constructor(private httpClient: HttpClient, private authenticationService: AuthenticationService) { }
+
+
+
+
+  getUsers(data: any):Promise <any>{
+
+    const httpHeaders = new HttpHeaders();
+
+    httpHeaders.append( "x-auth" , this.authenticationService.getToken() )
+
+    if(!data){
+
+      return this.httpClient.post( environment.host + environment.apiPath + "/users" ,{
+
+        headers: httpHeaders,
+
+      }).toPromise();
+
+    }else{
+
+
+
+    }
+
+    return this.httpClient.post( environment.host + environment.apiPath + "/users", data).toPromise();
+
+     
+
+    };
+
+
 
 }
-
-
