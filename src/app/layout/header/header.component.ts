8add7b4a50e82dbb2692bb54e44c17e1927b5df1
KAMZ13 <84303083+KAMZ13@users.noreply.github.com>
2021-11-10 00:32:45 -0600

    logOut

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/common/services/authentication.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authenticationService : AuthenticationService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+
+
+  logOut(): void{
+    this.authenticationService.clear();
+    this.router.navigate(['/login']);
+  }
 }
