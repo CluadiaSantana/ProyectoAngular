2b91258da8125191f73dba341c8da689327d3baa
Carlos Romo <79283213+CarlosRomo0806@users.noreply.github.com>
2021-11-10 01:33:53 -0600

    Sidebar corregido

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,20 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Attribute, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthenticationService } from 'src/app/common/services/authentication.service';
 
 @Component({
-  selector: 'app-header',
+  selector: 'app-header', 
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+
+  checked: boolean = false;
 
   constructor(private authenticationService : AuthenticationService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
-
+  hideMenu(): void{
+     this.checked = false;
+  }
 
   logOut(): void{
     this.authenticationService.clear();
