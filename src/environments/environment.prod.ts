0982c1787d18e14532b3385f7144c0b9cba9907f
KAMZ13 <84303083+KAMZ13@users.noreply.github.com>
2021-12-06 11:06:16 -0600

    created server.js cnttion hero-frontend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  host: "http://localhost:3001",
+  host: "https://bateria-desde-cero.herokuapp.com",
   apiPath: "/api",
   authPath: "/auth",
   GoogleProvider: "959886331117-k83jcgut24jgeonjlhfqo1vfflcmjale.apps.googleusercontent.com",
-  socketurl:"http://localhost:3001"
+  socketurl:"https://bateria-desde-cero.herokuapp.com"
 };
