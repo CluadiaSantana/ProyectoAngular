4b704f10f6c33e1b295192fb929926ec4782ac1d
KAMZ13 <84303083+KAMZ13@users.noreply.github.com>
2021-12-06 11:52:00 -0600

    stepsrequiredtoconnecttohero4

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express');
+const path = require ('path');
 
 const app = express();
 
 const port = process.env.PORT || 3000;
-
-const path = require ('path');
 
 
 app.use(express.static(path.join(__dirname,'dist','ProyectoAngular')));
